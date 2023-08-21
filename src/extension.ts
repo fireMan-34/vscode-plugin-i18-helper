ccd3a5e0b12333c19b2a8f58e86e3e4c5b24637e
maxufeng <unf-apqb330d4@dingtalk.com>
2023-08-21 19:06:38 +0800

    ✨ feat: 国际化调用代码生成命令
    简单实现生成国际化命令待调整 参考 1. https://juejin.cn/post/6844903775900024845#heading-5

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,17 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { range } from 'lodash';
+
+/** 起始结束映射行属性 */
+const rangeFix = (start: number, end: number) => {
+	return start === end ? [start] : range(start, end);
+};
+/** 从代码里解析对象 */
+const parseAttributeTexts2Object = (text: string) => {
+	const evalCode = `({${text}})`;
+	return eval(evalCode);
+};
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -13,12 +24,50 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('i18-extension.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('i18.formatMessage', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from i18-extension!');
-	});
 
+		const emptyWarning = (msg?: string) => {
+			vscode.window.showWarningMessage(`当前${msg || '上下文'}为空`);
+		};
+
+		// * 任务分解
+		// * 1. 获取选中代码 键值对 - [x]
+		// * 2. 映射快捷后代码 -[x]
+		// * 3. 映射到剪贴板 -[x]
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			emptyWarning();
+			return;
+		};
+		/** 当前文档对象 */
+		const document = editor.document;
+		/** 当前行对象 */
+		const { selection } = editor;
+		/** 起止行数 */
+		const line = {
+			start: selection.start.line,
+			end: selection.end.line,
+		};
+		const rangeLine = rangeFix(line.start, line.end);
+		
+		const formatMessage =  rangeLine.map((l, i) => {
+			const keyAndValText = document.lineAt(l).text;
+			const object = parseAttributeTexts2Object(keyAndValText);
+
+			const formatMessageFromLine = Object.entries(object).map(([i18Key, i18Msg]) => `formatMessage({
+				id: '${i18Key}',
+				defaultMessage: '${i18Msg}',
+			})`).join('');
+
+			return formatMessageFromLine;
+		}).join('');
+
+		const clipboard = vscode.env.clipboard;
+		clipboard.writeText(formatMessage);
+		vscode.window.showInformationMessage('国际化代码已经生成到剪切板');
+	});
 	context.subscriptions.push(disposable);
 }
 
