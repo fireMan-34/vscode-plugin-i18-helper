307b1db12a31ee42212774958807e11e97e7353f
maxufeng <unf-apqb330d4@dingtalk.com>
2023-08-23 14:30:11 +0800

    🔧 build: 添加 webpack 构建支持
    1. 修复 ts-alias 无法生成 编译时路径问题  2. 添加各种编译命令 3. 部分代码包括react 代码打包

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const { join } = require('path');
+const { cwd } = require('process');
+
+/** @type {import('webpack').Configuration} */
+const webpackConfig = {
+	mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
+  target: 'node', // VS Code extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
+  devtool: 'source-map',
+  entry: join(__dirname, './src/extension.ts'),
+  resolve: {
+    alias: {
+      commands: join(__dirname, './src/commands'),
+      utils: join(__dirname, './src/utils/'),
+    },
+    extensions: [ ".ts", '.js' , ".json" ]
+  },
+  externals: {
+    vscode: 'commonjs vscode' // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+    // modules added here also need to be added in the .vscodeignore file
+  },
+  output: {
+    filename: 'extension.js',
+    path: join(__dirname, 'out'),
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts?)$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ]
+  },
+  plugins: [
+  ],
+};
+
+module.exports = [ webpackConfig ];
