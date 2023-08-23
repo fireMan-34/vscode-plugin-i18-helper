c2cb7d166a93f7f59e7d3446c52def655db07dc9
maxufeng <unf-apqb330d4@dingtalk.com>
2023-08-23 14:33:20 +0800

    🦄 refactor: 移除命令行多余的函数嵌套

--- src/extension.ts
+++ src/extension.ts
@@ -15,11 +15,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('i18.parseKeyAndValue2FormatMessage', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		parseKeyAndValue2FormatMessage();
-	});
+	let disposable = vscode.commands.registerCommand('i18.parseKeyAndValue2FormatMessage',parseKeyAndValue2FormatMessage);
 	context.subscriptions.push(disposable);
 }
 
