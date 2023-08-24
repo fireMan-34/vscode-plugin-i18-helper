9b7e7550e72adc29d870e47d89cf670727bd33a8
maxufeng <unf-apqb330d4@dingtalk.com>
2023-08-24 14:16:55 +0800

    🐳 chore: 添加版本发布命令占位

---
+++ scripts/preVersion.ps1
@@ -0,0 +1,9 @@
+[System.Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+
+Write-Output "---------------------------------------------------"
+
+Write-Output "查看当前 npm 版本环境"
+
+Write-Output "npm preXXX 都是预执行脚本"
+
+Write-Output "---------------------------------------------------"
