184222f2daa015881ebb6566cc2d435490d23e12
fireMan-34 <524915379@qq.com>
2023-09-04 19:37:45 +0800

    🧪 test: 添加匹配正则文本测试

--- src/test/suite/code.test.ts
+++ src/test/suite/code.test.ts
@@ -1,25 +1,35 @@
-import { equal } from 'node:assert';
+import { equal, } from 'node:assert';
 import { describe, it, } from 'mocha';
-import { getCharsI18nType } from 'utils/code';
+import { getCharsI18nType, FORMAT_MESSAGE_ID_REGEX } from 'utils/code';
 import { I18nType } from 'types/index';
 
 describe('国际化文本检测测试', function () {
   it('中文繁体检测 1级', function () {
     equal(getCharsI18nType('應用管理'), I18nType.ZH_HK);
   });
-  it('中文繁体检测 2级', function() {
+  it('中文繁体检测 2级', function () {
     equal(getCharsI18nType('你正在刪除問題，問題刪除後該問題與其他問題的關聯關係也會取消，確認繼續嗎？'), I18nType.ZH_HK,);
   });
-  it('中文简体检测 1级', function() {
+  it('中文简体检测 1级', function () {
     equal(getCharsI18nType('搜索提示二'), I18nType.ZH_CN,);
   });
-  it('英文检测 1级', function() {
+  it('英文检测 1级', function () {
     equal(getCharsI18nType('Please press the "Refresh" button to reload current page'), I18nType.EN_US,);
   });
-  it('韩文检测 1级', function() {
+  it('韩文检测 1级', function () {
     equal(getCharsI18nType('상품의 가격 곡선을 표시할 수 있습니다.마우스를 가격 표시줄로 이동할 때 현재 규격 상품의 역사적 가격 변화를 표시합니다.'), I18nType.KO_KR,);
   });
-  it('日文检测 1级', function() {
+  it('日文检测 1级', function () {
     equal(getCharsI18nType('お問い合わせ中'), I18nType.JA_JP,);
+  });
+});
+
+describe('代码提示 formatMessage 系列', function () {
+  it('测试普通一行输入 匹配', function () {
+    equal(FORMAT_MESSAGE_ID_REGEX.test(`intl.formatMessage({ id: `), true);
+  });
+
+  it('普通测试一行 匹配相对多', function () {
+    equal(FORMAT_MESSAGE_ID_REGEX.test(`formatMessage({ id   :`), true);
   });
 });
