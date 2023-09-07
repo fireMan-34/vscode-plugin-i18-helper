8a37a0ebc8a28c12c9b52661af485eb6c89b79e8
fireMan-34 <524915379@qq.com>
2023-09-07 23:33:55 +0800

    🌈 style: 移除多余换行符

--- src/constants/provider.ts
+++ src/constants/provider.ts
@@ -11,13 +11,13 @@ export const SUPPORT_DOCUMENT_SELECTOR: DocumentSelector = [
 /** 生成代码模板类型 */
 export const GENERATE_TEMPLATE_MAP = {
     /** formatMessage 渲染国际化 key&val */
-    FORMAT_MESSSAGE_WITH_KEY_VAL: "formatMessage({\nid: '${id}',\ndefaultMessage: '${msg}',\n})\n",
+    FORMAT_MESSSAGE_WITH_KEY_VAL: "formatMessage({\nid: '${id}',\ndefaultMessage: '${msg}',\n})",
     /** formatMessage 渲染国际化 key */
-    FORMAT_MESSAGE_WITH_KEY: "formatMessage({\nid: '${id}',\n})\n",
+    FORMAT_MESSAGE_WITH_KEY: "formatMessage({\nid: '${id}',\n})",
     /** formatMessage 渲染国际化 key&val， 添加 intl 前缀 */
-    INT_FORMAT_MESSAGE_WITH_KEY_VAL: "intl.formatMessage({\nid: '${id}',\ndefaultMessage: '${msg}',\n})\n",
+    INT_FORMAT_MESSAGE_WITH_KEY_VAL: "intl.formatMessage({\nid: '${id}',\ndefaultMessage: '${msg}',\n})",
     /** formatMessage 渲染国际化 key， 添加 intl 前缀 */
-    INT_FORMAT_MESSAGE_WITH_KEY: "intl.formatMessage({\nid: '${id}',\n})\n",
+    INT_FORMAT_MESSAGE_WITH_KEY: "intl.formatMessage({\nid: '${id}',\n})",
     /** i18n.t 渲染国际化 key， 添加 intl 前缀 */
     I18N_T_WITH_KEY: "i18n.t('${id}')",
     /** t 渲染国际化 key， 添加 intl 前缀 */
