7f814691f2bdf11d29123df434ae5ecc88410870
maxufeng <unf-apqb330d4@dingtalk.com>
2023-09-07 14:50:00 +0800

    ✨ feat: 添加渲染模板字符串常量

--- src/constants/provider.ts
+++ src/constants/provider.ts
@@ -7,3 +7,20 @@ export const SUPPORT_DOCUMENT_SELECTOR: DocumentSelector = [
     { language: 'typescriptreact', scheme: 'file', },
     { language: 'javascriptreact', scheme: 'file', },
 ];
+
+/** 生成代码模板类型 */
+export const GENERATE_TEMPLATE_MAP = {
+    /** formatMessage 渲染国际化 key&val */
+    FORMAT_MESSSAGE_WITH_KEY_VAL: "formatMessage({\nid: '${id}',\ndefaultMessage: '${msg}',\n})\n",
+    /** formatMessage 渲染国际化 key */
+    FORMAT_MESSAGE_WITH_KEY: "formatMessage({\nid: '${id}',\n})\n",
+    /** formatMessage 渲染国际化 key&val， 添加 intl 前缀 */
+    INT_FORMAT_MESSAGE_WITH_KEY_VAL: "intl.formatMessage({\nid: '${id}',\ndefaultMessage: '${msg}',\n})\n",
+    /** formatMessage 渲染国际化 key， 添加 intl 前缀 */
+    INT_FORMAT_MESSAGE_WITH_KEY: "intl.formatMessage({\nid: '${id}',\n})\n",
+    /** i18n.t 渲染国际化 key， 添加 intl 前缀 */
+    I18N_T_WITH_KEY: "i18n.t('${id}')",
+    /** t 渲染国际化 key， 添加 intl 前缀 */
+    $T_WITH_KEY: "$t('${id}')",
+
+};
