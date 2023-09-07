a8779b2499b68f02492b7843f09747cccf8146f0
fireMan-34 <524915379@qq.com>
2023-09-07 21:14:18 +0800

    ✨ feat: 添加插件部分常量

--- src/constants/project.ts
+++ src/constants/project.ts
@@ -1,2 +1,6 @@
 /** @name 插件包名字 */
 export const EXTENSION_NAME = 'i18n-extension';
+/** @name 上传者 */
+export const PUBLISHER = 'fireMan-34';
+/** @name 插件 id */
+export const ENTENSION_ID = [EXTENSION_NAME, PUBLISHER ].join('.');
