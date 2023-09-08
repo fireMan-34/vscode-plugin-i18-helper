3b6e8e5d1dd952e346aa3dbbe4e0a26188067935
maxufeng <unf-apqb330d4@dingtalk.com>
2023-09-08 13:50:41 +0800

    ✨ feat: 添加 vscode 单元测试别名路径支持
    注意编写模块不要包含 vscode 相关模块，因为这个模块只是一个环境，不提供真实逻辑模块

--- src/test/unit/index.ts
+++ src/test/unit/index.ts
@@ -1,5 +1,6 @@
 import { equal } from 'assert';
 import { describe, it, } from 'mocha';
+import { isSameTrimString } from 'utils/str';
 
 function simpleTest (n: number) {
   return n**n;
@@ -8,6 +9,10 @@ function simpleTest (n: number) {
 describe('单元测试', function () {
   it('简单运行环境测试', function() {
     equal(simpleTest(2), 4);
+  });
+
+  it('测试是否能够正常识别别名路径', function(){
+    equal(isSameTrimString(__dirname, __dirname), true);
   });
 });
 
