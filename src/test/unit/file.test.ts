fec890d8910b268db240012075ca4db6f734816b
fireMan-34 <524915379@qq.com>
2023-09-09 13:11:10 +0800

    🧪 test: 获取子目录功能测试

--- src/test/unit/file.test.ts
+++ src/test/unit/file.test.ts
@@ -1,7 +1,7 @@
 import { deepEqual } from 'assert';
 import { join } from 'path';
 import { describe, it, } from 'mocha';
-import { checkPathIsFileOrDirectory, type FileAndDirPaths } from 'utils/fs';
+import { checkPathIsFileOrDirectory, type FileAndDirPaths, getSubDirectoryFromDirectoryPath } from 'utils/fs';
 
 const path = join(__dirname, '..');
 const suit = join(path, 'suite');
@@ -9,13 +9,19 @@ const unit = join(path, 'unit');
 const testPath = [ path, suit, unit ];
 
 let dirOrFilePaths: FileAndDirPaths;
+let subDirPaths: string[];
 
 beforeEach(async function() {
     dirOrFilePaths = await checkPathIsFileOrDirectory(testPath.concat(__filename));
+    subDirPaths = await getSubDirectoryFromDirectoryPath(path);
 });
 
 describe("测试文件类型模块", function() {
     it('测试路径识别功能', function () {
         deepEqual(dirOrFilePaths.dirPaths, testPath);
+    });
+
+    it('测试获取子目录功能', function() {
+        deepEqual(subDirPaths, [ suit, unit  ]);
     });
 });
