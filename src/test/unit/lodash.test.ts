38377cd775db382e7d11280c2225746532c767ea
fireMan-34 <524915379@qq.com>
2023-09-11 20:53:49 +0800

    🧪 test: lodash omit test 用例
    其实是自己写的太长了，搞错了变量，所以返回和输出不一样，😅

---
+++ src/test/unit/lodash.test.ts
@@ -0,0 +1,18 @@
+import { describe, it } from 'mocha';
+import { deepEqual } from 'assert';
+import { omit } from 'lodash';
+import { PROJECT_META_JSON, VSCODE_KEYS } from 'constants/index';
+import { ProjectGlobalConfig } from 'types/index';
+
+describe('lodash 疑问功能测试', function() {
+    it('omit 挑选对象确认', function() {
+        const projectConfig: ProjectGlobalConfig = {
+            ...PROJECT_META_JSON,
+            generateTemplate: '测试',
+            isOpenCheckDir: true,
+            mainLanguage: 'ZH_HK',
+        };
+
+        deepEqual(omit(projectConfig, VSCODE_KEYS), PROJECT_META_JSON);
+    });
+});
