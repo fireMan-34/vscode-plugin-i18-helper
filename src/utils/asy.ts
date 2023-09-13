e0e17bdb7c94c17d69994645b1b9bb78a105cb1c
fireMan-34 <524915379@qq.com>
2023-09-14 01:16:29 +0800

    ✨ feat: 工具库异步链式返回

--- src/utils/asy.ts
+++ src/utils/asy.ts
@@ -2,6 +2,10 @@ import assign from 'lodash/assign';
 
 type PickPromise<P> = P extends Promise<infer R> ? R : P;
 
+interface PromiseFunction<T> {
+  (): Promise<T>;
+}
+
 /** 映射 map 类型 promise */
 export function PromiseAllMap<T extends Record<string, Promise<any>>, K extends keyof T>(map: T): Promise<{ [key in K]: PickPromise<T[K]> }> {
   return Promise.all(
@@ -17,4 +21,18 @@ export function sleep(time: number = 1000): Promise<number>{
   return new Promise((resolve) => {
     setTimeout(() => { resolve(time); }, time);
   });
+};
+
+/** 异步链式返回 */
+export async function asyncChain<T>(asyncTasks: PromiseFunction<T>[]) {
+  for (let asyncTask of asyncTasks) {
+    try {
+      const result = await asyncTask();
+      return result;
+    } catch (err) {
+      continue;
+    }
+  }
+
+  return Promise.reject('nothing async task work can resolve');
 };
