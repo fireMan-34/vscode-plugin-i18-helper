e4e3fbbe5e8ff411f6deb8932e562d485650c1df
maxufeng <unf-apqb330d4@dingtalk.com>
2023-09-14 15:09:14 +0800

    🧪 test: 路径检测测试

--- src/test/unit/file.test.ts
+++ src/test/unit/file.test.ts
@@ -1,5 +1,6 @@
-import { deepEqual } from 'assert';
+import { deepEqual, equal } from 'assert';
 import { join } from 'path';
+import { existsSync } from 'fs';
 import { describe, it, } from 'mocha';
 import { checkPathIsFileOrDirectory, type FileAndDirPaths, getSubDirectoryFromDirectoryPath } from 'utils/fs';
 
@@ -23,5 +24,9 @@ describe("测试文件类型模块", function() {
 
     it('测试获取子目录功能', function() {
         deepEqual(subDirPaths, [ suit, unit  ]);
+    });
+
+    it('测试路径是否存在: 路径不存在', function() {
+        equal(existsSync("d:\\Study\\i18n-extension\\.i18n\\fa5dafcd319fc9e446121a358bb0c46b6e11b9c6ea1acd820e21e3b6d23ac9f5\\meta.json"), false);
     });
 });
