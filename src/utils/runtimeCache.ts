d1da5ec71a8164a23c30d1c277ddc8971ef7bf16
maxufeng <unf-apqb330d4@dingtalk.com>
2023-09-15 16:10:47 +0800

    ✨ feat: 添加工具库定时移除 key 功能

--- src/utils/runtimeCache.ts
+++ src/utils/runtimeCache.ts
@@ -1,6 +1,18 @@
 const GLOBAL_RUN_TIME_CACHE: Record<string, any> = {};
 
-export const createRunTimeCahce = (namespace?: string) => {
+interface CreateRunTimeCahceOptions {
+  /** 自动刷新时间
+   * if has it , cache will remove sometime when over remove settime
+   */
+  autoRemoveTime?: number;
+}
+
+export const createRunTimeCahce = (namespace?: string, options?: CreateRunTimeCahceOptions) => {
+  const {
+    autoRemoveTime,
+  } = {
+    ...options,
+  };
   if (namespace) {
     GLOBAL_RUN_TIME_CACHE[namespace] = {};
   }
@@ -11,6 +23,9 @@ export const createRunTimeCahce = (namespace?: string) => {
       return cache[key];
     },
     setKey: (key: string | number, value: any) => {
+      if (autoRemoveTime) {
+        setTimeout(() => ctl.clearKey(key), autoRemoveTime);
+      }
       return cache[key] = value;
     },
     clearKey: (key: string | number) => {
