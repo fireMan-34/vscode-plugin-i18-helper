71d9848f9ef9649556da13b7238b635ce9dbc320
fireMan-34 <524915379@qq.com>
2023-12-19 23:18:45 +0800

    🧪 test: 添加 ts 插入属性列表对应测试

--- src/test/unit/type/index.test.ts
+++ src/test/unit/type/index.test.ts
@@ -17,6 +17,7 @@ import {
 } from "typescript";
 import { expect, } from 'chai';
 import typeJson from './type.json';
+import { SyntaxKind } from "ts-morph";
 
 declare namespace ts {
   interface StringLiteral {
@@ -85,13 +86,14 @@ describe("学习测试 typescript API", function () {
     expect(outFile, '输出结果不为空').is.not.null;
     expect(outFile, '包含生成代码文本').has.string(addKey);
     expect(outFile, '包含生成代码文本').has.string(addVal);
+    transformSourceFile.dispose();
   });
 
   if (!sourceJsonFile) {
     return;
   }
+  const modifyKey = '$schema';
   it('测试 json 格式解析和修改', function () {
-    const modifyKey = '$schema';
     const transformResult = transform(sourceJsonFile, [
       (context) => {
         return (sourceFile) => {
@@ -125,5 +127,66 @@ describe("学习测试 typescript API", function () {
     // 无法直接生成 json 格式只能用这种方式取巧
     const json = eval(outFile);
     expect(json).ownProperty(modifyKey).has.property(addKey).is.string(addVal);
+    transformResult.dispose();
+    });
+
+  it('测试某个 json 属性新增在某个属性位置前面', function (){
+    const transformResult = transform(
+      sourceJsonFile,
+      [
+        (context) => {
+          const visit = (node: Node) => {
+            node = visitEachChild(node, visit, context);
+            const { factory, } = context; 
+            if (isObjectLiteralExpression(node)) {
+              const hasFindIndex = node.properties.findIndex(p => p.name && isStringLiteral(p.name) && p.name.text === modifyKey );
+              if (hasFindIndex > -1) {
+                return factory.updateObjectLiteralExpression(
+                  node,
+                  factory.createNodeArray(
+                    node.properties.slice(0, hasFindIndex)
+                    .concat(factory.createPropertyAssignment(
+                      addKey,
+                      factory.createStringLiteral(addVal),
+                    ))
+                    .concat(node.properties.slice(hasFindIndex)),
+                  ),
+                );
+              }
+            }
+            return node;
+          };
+          return (sourceFile) => {
+            return visitNode(sourceFile, visit) as SourceFile;
+          };
+        }
+      ],
+    );
+    const [nextSourceFile]  = transformResult.transformed;
+
+    const testVisit  = (cn: Node) => {
+      cn.forEachChild(testVisit);
+      if (!isObjectLiteralExpression(cn)) {
+        return;
+      }
+      const modifyIndex = cn.properties.findIndex(p => 
+        p.name
+        && isStringLiteral(p.name)
+        && p.name.text === modifyKey
+      );
+      if (modifyIndex <= -1) {
+        return;
+      }
+      const addKeyIndex = cn.properties.findIndex(p =>
+        p.name
+        // 记住 如果没有构造实例的 name 默认是标识符的形式
+        && (isStringLiteral(p.name) || isIdentifier(p.name))
+        && p.name.text === addKey
+      );
+      expect(addKeyIndex).gt(-1);
+      expect(addKeyIndex).lt(modifyIndex);
+    };
+
+    nextSourceFile.forEachChild(testVisit);
   });
 });
