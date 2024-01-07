40e5d546761db35a6291624e7cdbff4ce461efee
fireMan-34 <524915379@qq.com>
2024-01-07 14:47:37 +0800

    ✨ feat: 自定义跳转 ast 模式

---
+++ src/providers/define.1.ts
@@ -0,0 +1,43 @@
+import type { DefinitionProvider, ExtensionContext } from 'vscode';
+import { Location, Uri, languages, workspace, } from 'vscode';
+
+import { SUPPORT_DOCUMENT_SELECTOR } from 'constants/index';
+import { I18nDbPaser, } from 'models/i18nDbParser';
+import { I18nGenTemplate } from 'models/i18nGenTemplate';
+import { I18nType } from 'types/index';
+import { getGlobalConfigurationSync } from 'utils/conf';
+import { getWrokspaceFloder } from 'utils/path.code';
+
+/** 定义跳转 2.0 版本 */
+const definitionProvider: DefinitionProvider = {
+    async provideDefinition(document, position, token) {
+        const i18nGenTemplatee = new I18nGenTemplate().refreshTemplateModals();
+        const i18nId = i18nGenTemplatee.getI18nIdFromDocumentPosition(document, position);
+        if (!i18nId) {
+            return;
+        }
+        const globalConfig = getGlobalConfigurationSync();
+        const workspaceFolder = await getWrokspaceFloder({
+            multiplySelect: 'matchFile',
+            matchPath: document.uri,
+        });
+        if (!workspaceFolder) {
+            return;
+        }
+        const i18nDbParser = new I18nDbPaser(globalConfig, workspaceFolder);
+        i18nDbParser.prepareCheck();
+        const pathMaps = await i18nDbParser.findKeyOrValue(i18nId, void 0, 'key');
+        const { mainLanguage, } = globalConfig;
+        const mainI18nData = pathMaps[I18nType[mainLanguage]];
+        if (!mainI18nData) {
+            return;
+        }
+        const [key, value, item] = mainI18nData;
+        const dbDocument = await workspace.openTextDocument(item.path);
+        const dbContent = dbDocument.getText();
+        const pos = dbDocument.positionAt(dbContent.search(new RegExp(`("|')?${key}\\1?`)));
+        return new Location(Uri.file(item.path), pos);
+    },
+};
+
+export const createAstDefinitionProvider = (context: ExtensionContext) => languages.registerDefinitionProvider(SUPPORT_DOCUMENT_SELECTOR, definitionProvider);
--- src/providers/index.ts
+++ src/providers/index.ts
@@ -1,21 +1,23 @@
 
 import { ExtensionContext } from 'vscode';
 import { createI18nCompetionItemProvider } from 'providers/completionItem';
-import { createDefinitionProvider } from 'providers/define';
+// import { createDefinitionProvider } from 'providers/define';
 // import { createHoverProvider } from 'providers/hover';
 import { createTreeI18nMapDirProvider } from 'providers/i18nMapDir';
 import { createI18nMapConfig } from 'providers/i18nMapConfig';
 import createAstHoverProvider from 'providers/hover.1';
+import { createAstDefinitionProvider } from 'providers/define.1';
 
 /** 插件 i18n 提供服务 */
 export const createI18nProvider = (context: ExtensionContext) => {
     [
         createI18nCompetionItemProvider,
-        createDefinitionProvider,
+        // createDefinitionProvider,
         // createHoverProvider,
         createTreeI18nMapDirProvider,
         createI18nMapConfig,
         createAstHoverProvider,
+        createAstDefinitionProvider,
     ].forEach(registerProvider => {
         context.subscriptions.push(registerProvider(context));
     });
